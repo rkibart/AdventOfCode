3c1d102a6e468075eed3217382113f5e1fca1521
rkibart <r.kibart@gmail.com>
2022-12-05 18:00:21 +0100

    day 1

---
+++ day-01.php
@@ -0,0 +1,50 @@
+<?php
+
+$data = file( 'input-01.txt', FILE_IGNORE_NEW_LINES );
+
+$data[] = '';
+
+$dataSize = count($data);
+
+/* --- Part One --- */
+
+$firstPartResult = 0;
+$elfCalories = 0;
+
+for($i = 0; $i < $dataSize; $i++ ) {
+
+    if( $data[$i] !== '' ) {
+        $elfCalories += (int) $data[ $i ];
+
+    } else {
+        
+        $firstPartResult = $elfCalories > $firstPartResult ? $elfCalories : $firstPartResult;
+        $elfCalories = 0;
+    }
+}
+
+echo "<b>$firstPartResult</b><br />";
+
+/* --- Part Two --- */
+
+$topThreeElves = [0, 0, 0];
+$elfCalories = 0;
+
+for($i = 0; $i < $dataSize ; $i++ ) {
+
+    if( $data[$i] !== '' ) {
+
+        $elfCalories += (int) $data[ $i ];
+
+    } else {
+        
+        $topThreeElves[0] = $elfCalories > $topThreeElves[0] ? $elfCalories : $topThreeElves[0];
+        sort( $topThreeElves );
+        $elfCalories = 0;
+
+    }
+}
+
+$secondPartResult = array_sum( $topThreeElves );
+
+echo "<b>$secondPartResult</b><br />";
