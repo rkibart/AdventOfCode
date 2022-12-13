d670aee3894b4b27043d09bb0e213b4475ca4a65
rkibart <r.kibart@gmail.com>
2022-12-13 16:58:20 +0100

    day 10

---
+++ day-10.php
@@ -0,0 +1,66 @@
+<?php
+$data = file( 'input-10.txt', FILE_IGNORE_NEW_LINES );
+foreach( $data as &$row ) {
+    $row = explode( ' ', $row );
+}
+
+$cycle = 1;
+$xRegister = 1;
+$drawedCRTLines = array_fill(0, 6, '');
+$signalStrength = 0;
+
+foreach($data as $instructionLine ) {
+    $instrunction = $instructionLine[0];
+    $value = $instructionLine[1] ?? null;
+
+    switch( $instrunction ) {
+
+        case 'noop':
+            
+            if( $cycle % 40 === 20) {
+
+                $signalStrength += $cycle * $xRegister;
+            }
+
+            beforeCycleTick($cycle, $xRegister, $drawedCRTLines);
+            $cycle++;
+            break;
+        case 'addx':
+            if( $cycle % 40 === 20) {
+                
+                $signalStrength += $cycle * $xRegister;
+            }
+            beforeCycleTick($cycle, $xRegister, $drawedCRTLines);
+            $cycle++;
+            if( $cycle % 40 === 20) {
+
+                $signalStrength += $cycle * $xRegister;
+            }
+            beforeCycleTick($cycle, $xRegister, $drawedCRTLines);
+            $cycle++;
+            $xRegister += $value;
+            break;
+    }
+}
+
+function  beforeCycleTick( int $cycle, int $register, array &$printArray): void {
+
+    $line = floor( ($cycle - 1) / 40 );
+    $position = $cycle-1 - 40 * $line;
+
+    if( $position >= $register - 1 && $position <= $register + 1 ) {
+        $printArray[$line] .= '#';
+    }else {
+        $printArray[$line] .= '_';
+    }
+}
+/* --- Part One --- */
+
+$firstPartResult = $signalStrength;
+echo "<b>$firstPartResult</b><br />";
+
+/* --- Part Two --- */
+
+foreach($drawedCRTLines as $line ) {
+    echo $line . '<br />';
+}
